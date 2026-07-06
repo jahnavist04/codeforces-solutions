c0ae8830bf10b87c82e29579548082cf6ab15df3
JAHNAVI S T <jahnavist5@gmail.com>
2026-07-06 19:07:17 +0530

    Add LeetCode/Medium/7-reverse-integer/try-1.cpp

---
+++ LeetCode/Medium/7-reverse-integer/try-1.cpp
@@ -0,0 +1,26 @@
+/*
+ * Problem #7: Reverse Integer
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 7/5/2026, 7:59:51 PM
+ * Link: https://leetcode.com/problems/reverse-integer/
+ */
+
+class Solution {
+public:
+    int reverse(int x) {
+        
+        int ans = 0;
+        while(x != 0) {
+            int digit = x%10;
+            if( (ans>INT_MAX/10) || (ans<INT_MIN/10)) {
+                return 0;
+            }
+            ans = (ans*10) + digit;
+            x = x/10;
+        }
+        return ans;
+    }
+};
