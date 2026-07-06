e996a6071a30d8e33acd97c8c0891dd62d48acdc
JAHNAVI S T <jahnavist5@gmail.com>
2026-07-06 19:07:18 +0530

    Add Codeforces/2240/A/try-1.py

---
+++ Codeforces/2240/A/try-1.py
@@ -0,0 +1,24 @@
+/*
+ * Problem : Another Popcount Problem
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: Python 3
+ * Date: 7/6/2026, 7:01:58 PM
+ * Link: https://codeforces.com/contest/2240/problem/A
+ */
+
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+
+    ans = 0
+
+    for i in range(31):
+        cost = 1 << i
+        take = min(n // cost, k)
+        ans += take
+        n -= take * cost
+
+    print(ans)
