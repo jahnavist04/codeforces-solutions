5f4726b8e0c6430ab04fbc9b33b0e38c522fb190
JAHNAVI S T <jahnavist5@gmail.com>
2026-07-09 19:42:03 +0530

    Add Codeforces/4/A/try-1.py

---
+++ Codeforces/4/A/try-1.py
@@ -0,0 +1,16 @@
+/*
+ * Problem : Watermelon
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: Python 3
+ * Date: 7/9/2026, 7:38:49 PM
+ * Link: https://codeforces.com/contest/4/problem/A
+ */
+
+w = int(input())
+
+if w > 2 and w % 2 == 0:
+    print("YES")
+else:
+    print("NO")
