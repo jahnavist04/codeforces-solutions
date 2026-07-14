ef26976173c7e5dd78b73d9cec742e03e7d6ca36
JAHNAVI S T <jahnavist5@gmail.com>
2026-07-14 12:41:19 +0530

    Add Codeforces/71/A/try-1.py

---
+++ Codeforces/71/A/try-1.py
@@ -0,0 +1,19 @@
+/*
+ * Problem : Way Too Long Words
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: Python 3
+ * Date: 7/14/2026, 12:39:43 PM
+ * Link: https://codeforces.com/contest/71/problem/A
+ */
+
+n = int(input())
+
+for _ in range(n):
+    word = input()
+
+    if len(word) <= 10:
+        print(word)
+    else:
+        print(word[0] + str(len(word) - 2) + word[-1])
