f1a0a4a785d60a910db700109d63b7ba5e56782d
JAHNAVI S T <jahnavist5@gmail.com>
2026-07-26 19:49:07 +0530

    Add Codeforces/282/A/try-2.py

---
+++ Codeforces/282/A/try-2.py
@@ -0,0 +1,22 @@
+/*
+ * Problem : Bit++
+ * Difficulty: Easy
+ * Submission: Try 2
+ * status: Accepted
+ * Language: Python 3
+ * Date: 7/26/2026, 7:43:24 PM
+ * Link: https://codeforces.com/contest/282/problem/A
+ */
+
+n = int(input())
+
+x = 0
+
+for _ in range(n):
+    statement = input()
+    if "++" in statement:
+        x += 1
+    else:
+        x -= 1
+
+print(x)
