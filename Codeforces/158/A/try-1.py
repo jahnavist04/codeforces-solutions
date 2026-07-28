d680299aca2dbc71a8024dffb36caa514f54f3af
JAHNAVI S T <jahnavist5@gmail.com>
2026-07-28 19:19:32 +0530

    Add Codeforces/158/A/try-1.py

---
+++ Codeforces/158/A/try-1.py
@@ -0,0 +1,21 @@
+/*
+ * Problem : Next Round
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: Python 3
+ * Date: 7/28/2026, 7:12:40 PM
+ * Link: https://codeforces.com/contest/158/problem/A
+ */
+
+n, k = map(int, input().split())
+scores = list(map(int, input().split()))
+
+cutoff = scores[k - 1]
+
+count = 0
+for score in scores:
+    if score >= cutoff and score > 0:
+        count += 1
+
+print(count)
