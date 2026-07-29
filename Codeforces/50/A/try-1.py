8c8c77e535f4026381039e0026d405f2a428155a
JAHNAVI S T <jahnavist5@gmail.com>
2026-07-29 21:55:05 +0530

    Add Codeforces/50/A/try-1.py

---
+++ Codeforces/50/A/try-1.py
@@ -0,0 +1,12 @@
+/*
+ * Problem : Domino piling
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: Python 3
+ * Date: 7/29/2026, 9:52:15 PM
+ * Link: https://codeforces.com/contest/50/problem/A
+ */
+
+m, n = map(int, input().split())
+print((m * n) // 2)
