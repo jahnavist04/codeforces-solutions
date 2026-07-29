8a65caaf6329ae7a8eefa3cc7d5b4757682f4d28
JAHNAVI S T <jahnavist5@gmail.com>
2026-07-29 21:53:53 +0530

    Add Codeforces/231/A/try-1.py

---
+++ Codeforces/231/A/try-1.py
@@ -0,0 +1,20 @@
+/*
+ * Problem : Team
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: Python 3
+ * Date: 7/27/2026, 7:37:35 PM
+ * Link: https://codeforces.com/contest/231/problem/A
+ */
+
+n = int(input())
+
+count = 0
+
+for _ in range(n):
+    p, v, t = map(int, input().split())
+    if p + v + t >= 2:
+        count += 1
+
+print(count)
