4799a797e9703bc7395c90637707c7cda80add28
JAHNAVI S T <jahnavist5@gmail.com>
2026-08-09 12:03:10 +0530

    Add LeetCode/Hard/4-median-of-two-sorted-arrays/try-2.java

---
+++ LeetCode/Hard/4-median-of-two-sorted-arrays/try-2.java
@@ -0,0 +1,33 @@
+/*
+ * Problem #4: Median of Two Sorted Arrays
+ * Difficulty: Hard
+ * Submission: Try 2
+ * status: Accepted
+ * Language: java
+ * Date: 8/7/2026, 10:03:12 AM
+ * Link: https://leetcode.com/problems/median-of-two-sorted-arrays/
+ */
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1 = nums1.length, n2 = nums2.length;
+        int[] nums3 = new int[n1 + n2];
+        int p1 = 0, p2 = 0, p3 = 0;
+        while (p1 < n1 && p2 < n2) {
+            if (nums1[p1] <= nums2[p2])
+                nums3[p3++] = nums1[p1++];
+            else
+                nums3[p3++] = nums2[p2++];
+        }
+        while (p1 < n1)
+            nums3[p3++] = nums1[p1++];
+        while (p2 < n2)
+            nums3[p3++] = nums2[p2++];
+        int n = nums3.length;
+        if (n % 2 == 1) {
+            return nums3[n / 2];
+        } else {
+            return (nums3[n / 2 - 1] + nums3[n / 2]) / 2.0;
+        }
+    }
+}
