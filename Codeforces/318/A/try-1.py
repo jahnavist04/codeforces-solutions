44443ac5dad72ec67119c93f0636106591a72f22
JAHNAVI S T <jahnavist5@gmail.com>
2026-09-05 19:48:35 +0530

    Add Codeforces/318/A/try-1.py

---
+++ Codeforces/318/A/try-1.py
@@ -0,0 +1,20 @@
+/*
+ * Problem : Even Odds
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: Python 3
+ * Date: 9/5/2026, 7:46:53 PM
+ * Link: https://codeforces.com/contest/318/problem/A
+ */
+
+n, k = map(int, input().split())
+
+odd_count = (n + 1) // 2
+
+if k <= odd_count:
+    answer = 2 * k - 1
+else:
+    answer = 2 * (k - odd_count)
+
+print(answer)
