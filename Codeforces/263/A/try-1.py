36623db6b2864a6f877b894ad24b508791161f5b
JAHNAVI S T <jahnavist5@gmail.com>
2026-09-05 19:48:34 +0530

    Add Codeforces/263/A/try-1.py

---
+++ Codeforces/263/A/try-1.py
@@ -0,0 +1,23 @@
+/*
+ * Problem : Beautiful Matrix
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: Python 3
+ * Date: 9/5/2026, 7:47:48 PM
+ * Link: https://codeforces.com/contest/263/problem/A
+ */
+
+row = col = 0
+
+for i in range(1, 6):
+    nums = list(map(int, input().split()))
+
+    for j in range(1, 6):
+        if nums[j - 1] == 1:
+            row = i
+            col = j
+
+moves = abs(row - 3) + abs(col - 3)
+
+print(moves)
