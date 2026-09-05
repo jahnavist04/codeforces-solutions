25b487a9ef77b7675d9715a2982eda7dade642a4
JAHNAVI S T <jahnavist5@gmail.com>
2026-09-05 19:48:10 +0530

    Add Codeforces/617/A/try-1.py

---
+++ Codeforces/617/A/try-1.py
@@ -0,0 +1,15 @@
+/*
+ * Problem : Elephant
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: Python 3
+ * Date: 8/8/2026, 6:20:50 PM
+ * Link: https://codeforces.com/contest/617/problem/A
+ */
+
+x = int(input())
+
+steps = (x + 4) // 5
+
+print(steps)
