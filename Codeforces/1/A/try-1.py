66ace4983145ccad627ff65cff994d05b74f8928
JAHNAVI S T <jahnavist5@gmail.com>
2026-09-05 19:48:05 +0530

    Add Codeforces/1/A/try-1.py

---
+++ Codeforces/1/A/try-1.py
@@ -0,0 +1,16 @@
+/*
+ * Problem : Theatre Square
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: Python 3
+ * Date: 8/6/2026, 7:41:07 PM
+ * Link: https://codeforces.com/contest/1/problem/A
+ */
+
+n, m, a = map(int, input().split())
+
+rows = (n + a - 1) // a
+cols = (m + a - 1) // a
+
+print(rows * cols)
